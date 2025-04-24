d4a993b6a4021873459b5fa83259312699ad476a
scry5 <72686328+scry5@users.noreply.github.com>
2025-04-25 00:02:07 +0100

    Make powerlevel hidden folder

--- run_onchange_install.sh
+++ run_onchange_install.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-if cd ~/powerlevel10k; then git pull; else git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k; fi
+if cd ~/.powerlevel10k; then git pull; else git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k; fi
 if ! command -v nvim 2>&1 >/dev/null; then pip3 install tldr; fi
 
 if [ "$(uname)" = "Darwin" ]; then
