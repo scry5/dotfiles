a3aab4555a2a305d99025e09ca473ae0a513dbac
scry5 <72686328+scry5@users.noreply.github.com>
2025-04-26 00:42:14 +0100

    Add fdfind, only run install scripts if package is not installed

--- install_scripts/linux.sh
+++ install_scripts/linux.sh
@@ -3,8 +3,13 @@ if ! command -v nvim 2>&1 >/dev/null; then
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
   sudo mv nvim.appimage ~/.local/bin/nvim
-else
-  echo "Neovim already installed. Skipping!"
 fi
-sudo apt-get install -y ripgrep
-sudo apt-get install -y bat
+if ! command -v rg 2>&1 >/dev/null; then
+  sudo apt-get install -y ripgrep
+fi
+if ! command -v batcat 2>&1 >/dev/null; then
+  sudo apt-get install -y bat
+fi
+if ! command -v fdfind 2>&1 >/dev/null; then
+  sudo apt-get install -y fd-find
+fi
