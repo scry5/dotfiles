c4af4ad7a57cb938508b4443a17d171fdce4a0cb
Kevin Xia <kevinxia@stripe.com>
2025-04-30 16:13:47 +0100

    comment out tldr for now

--- run_install.sh
+++ run_install.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 if cd ~/.powerlevel10k; then git pull; else git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k; fi
-if ! command -v nvim 2>&1 >/dev/null; then pip3 install tldr; fi
+#if ! command -v tldr 2>&1 >/dev/null; then pip3 install tldr; fi
 
 if [ "$(uname)" = "Darwin" ]; then
   bash "${CHEZMOI_WORKING_TREE}/install_scripts/mac.sh"
