fa15867768aef226957d4e38ecf25cbaae24d127
Kevin Xia <kevinxia@stripe.com>
2026-07-30 14:04:20 +0100

    Download rust

--- helpers/install_mac.sh
+++ helpers/install_mac.sh
@@ -5,3 +5,6 @@ brew install fd
 brew install bat
 brew install zellij
 brew install fzf
+if ! command -v cargo 2>&1 >/dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
